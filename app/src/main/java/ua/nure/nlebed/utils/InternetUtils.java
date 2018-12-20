e77911fb35f7fbe23ea3b055d14e3a0e7e377085
nlebed <nikita.lebed@dataart.com>
2018-12-20 19:00:44 +0200

    send ip address

---
+++ app/src/main/java/ua/nure/nlebed/utils/InternetUtils.java
@@ -0,0 +1,56 @@
+package ua.nure.nlebed.utils;
+
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.util.Collections;
+import java.util.List;
+
+public class InternetUtils {
+
+    public static String getMACAddress(String interfaceName) {
+        try {
+            List<NetworkInterface> interfaces = Collections.list(NetworkInterface.getNetworkInterfaces());
+            for (NetworkInterface aInterface : interfaces) {
+                if (interfaceName != null) {
+                    if (!aInterface.getName().equalsIgnoreCase(interfaceName)) continue;
+                }
+                byte[] mac = aInterface.getHardwareAddress();
+                if (mac == null) return "";
+                StringBuilder buf = new StringBuilder();
+                for (byte aMac : mac) buf.append(String.format("%02X:", aMac));
+                if (buf.length() > 0) buf.deleteCharAt(buf.length() - 1);
+                return buf.toString();
+            }
+        } catch (Exception ignored) {
+
+        }
+        return "";
+    }
+
+    public static String getIPAddress(boolean useIPv4) {
+        try {
+            List<NetworkInterface> interfaces = Collections.list(NetworkInterface.getNetworkInterfaces());
+            for (NetworkInterface intf : interfaces) {
+                List<InetAddress> addrs = Collections.list(intf.getInetAddresses());
+                for (InetAddress addr : addrs) {
+                    if (!addr.isLoopbackAddress()) {
+                        String sAddr = addr.getHostAddress();
+                        boolean isIPv4 = sAddr.indexOf(':') < 0;
+
+                        if (useIPv4) {
+                            if (isIPv4)
+                                return sAddr;
+                        } else {
+                            if (!isIPv4) {
+                                int delim = sAddr.indexOf('%'); // drop ip6 zone suffix
+                                return delim < 0 ? sAddr.toUpperCase() : sAddr.substring(0, delim).toUpperCase();
+                            }
+                        }
+                    }
+                }
+            }
+        } catch (Exception ignored) {
+        }
+        return "";
+    }
+}
--- app/src/main/java/ua/nure/nlebed/utils/JsonUtils.java
+++ app/src/main/java/ua/nure/nlebed/utils/JsonUtils.java
@@ -9,7 +9,8 @@ public class JsonUtils {
     private static final String ANDROID = "ANDROID";
 
     public static JSONObject createUserJson(GoogleSignInAccount account) throws JSONException {
-        String wlan0 = MacAddressUtils.getMACAddress("wlan0");
+        String wlan0 = InternetUtils.getMACAddress("wlan0");
+        String ipAddress = InternetUtils.getIPAddress(true);
         JSONObject userJson = new JSONObject();
         String email = account.getEmail();
         int indexOf = email.indexOf('.');
@@ -17,7 +18,7 @@ public class JsonUtils {
         userJson.put("name", email.substring(0, indexOf));
         userJson.put("lastName", email.substring(indexOf + 1, email.indexOf('@')));
         userJson.put("macAddress", wlan0);
-        userJson.put("ipAddress", "nothing");
+        userJson.put("ipAddress", ipAddress);
         userJson.put("device", ANDROID);
         userJson.put("photoUrl", account.getPhotoUrl());
         return userJson;
